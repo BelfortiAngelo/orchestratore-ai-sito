4e5cf9e459eeef9c385850e26a0c577ae7cc56f4
BelfortiAngelo <angelobelfo@gmail.com>
2026-05-17 21:03:18 +0200

    fix link LinkedIn

--- app.jsx
+++ app.jsx
@@ -116,7 +116,7 @@ function Nav() {
       <div className="nav-right">
         <a className="nav-link" href="#guide">Guide</a>
         <a className="nav-link" href="#kit">Starter Kit</a>
-        <a className="nav-cta" href="https://www.linkedin.com/in/angelobelforti" target="_blank" rel="noreferrer noopener">
+        <a className="nav-cta" href="https://www.linkedin.com/in/angelo-belforti-6a31891a8/" target="_blank" rel="noreferrer noopener">
           <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
             <path d="M20.45 20.45h-3.56v-5.57c0-1.33-.02-3.04-1.85-3.04-1.86 0-2.14 1.45-2.14 2.95v5.66H9.34V9h3.42v1.56h.05c.48-.9 1.64-1.85 3.37-1.85 3.6 0 4.27 2.37 4.27 5.46v6.28zM5.34 7.43a2.07 2.07 0 1 1 0-4.14 2.07 2.07 0 0 1 0 4.14zM7.12 20.45H3.56V9h3.56v11.45zM22.22 0H1.77C.79 0 0 .77 0 1.72v20.56C0 23.23.79 24 1.77 24h20.45c.98 0 1.78-.77 1.78-1.72V1.72C24 .77 23.2 0 22.22 0z" />
           </svg>
@@ -362,7 +362,7 @@ function Footer() {
         <div className="footer-meta">
           <span>2026 Angelo Belforti</span>
           <span className="footer-sep" />
-          <a href="https://www.linkedin.com/in/angelobelforti" target="_blank" rel="noreferrer noopener">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/angelo-belforti-6a31891a8/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
           <span className="footer-sep" />
           <a href="mailto:angelobelfo@gmail.com">Email</a>
         </div>
